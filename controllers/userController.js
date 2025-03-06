f1273eb0563450a7dd7c7e6c81d67e8e2d89e885
sandun440 <sameerasandun440@gmail.com>
2025-03-07 00:28:51 +0530

    create user deleteèfunction

--- controllers/userController.js
+++ controllers/userController.js
@@ -60,3 +60,11 @@ export function loginUser(req,res){
         }
     )
 }
+
+export function deleteUser(req,res){
+    User.deleteOne({email : req.body.email}).then(()=>{
+        res.json({
+            message : "User deleted"
+        })
+    })
+}
