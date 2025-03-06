15c4aebb8770aac7004f4f56a9451a3f820a12df
sandun440 <sameerasandun440@gmail.com>
2025-03-07 01:03:30 +0530

    Adding aditional attribute to the product schema

--- model/product.js
+++ model/product.js
@@ -3,7 +3,8 @@ import mongoose from "mongoose";
 const productSchema = mongoose.Schema({
     name : String,
     price : Number,
-    description : String
+    description : String,
+    lastPrice : Number
 })
 
 const Product = mongoose.model("products", productSchema)
