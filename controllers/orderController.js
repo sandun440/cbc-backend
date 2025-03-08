7b8ac4f54bde2bda0c0e575af203d98ca1493bfd
sandun440 <sameerasandun440@gmail.com>
2025-03-08 22:28:41 +0530

    save commit

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -45,3 +45,16 @@ try {
     });
 }
 }
+
+export async function getOrders(req, res) {
+    try {
+        const orders = await Order.find({ email: req.user.email });
+
+        res.json(orders);
+
+    }catch(error){
+        res.status(500).json({
+            message: error.message
+        });
+    }
+}
